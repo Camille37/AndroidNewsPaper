06a2c4cd132683fa9f6ad374b0e13227ab2cb666
CamilleA <123865377+Camille37@users.noreply.github.com>
2024-11-15 14:14:34 +0100

    Adding the go back option in the toolbar of article detail

--- app/src/main/java/com/example/newspaper/ArticleDetailActivity.java
+++ app/src/main/java/com/example/newspaper/ArticleDetailActivity.java
@@ -1,10 +1,15 @@
 package com.example.newspaper;
 
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import com.bumptech.glide.Glide;
 
 public class ArticleDetailActivity extends AppCompatActivity {
@@ -26,5 +31,24 @@ public class ArticleDetailActivity extends AppCompatActivity {
             detailDescription.setText(article.getDescription());
             Glide.with(this).load(article.getImageUrl()).into(detailImage);
         }
+
+        // Trouver la Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);  // Définir la Toolbar comme ActionBar
+
+        // Activer la flèche de retour (Up button)
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_back_arrow);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();  // Gérer le retour à l'activité précédente
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
+
